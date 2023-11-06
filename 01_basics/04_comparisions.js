77d3da181ab4e513c7f1774b622cbab7aa179684
alwaysricky <149997658+alwaysricky@users.noreply.github.com>
2023-11-06 11:15:09 +0000

    3 4 n 5

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,7 @@
+
+
+console.log('2' > 1)
+console.log('02' > 1)
+
+// copy the code from JS-HINDI-YOUTUBE (Repositry - Big Bucket)
+
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Hitesh"
+
+const repoCount = 50
+
+//console.log(name + repoCount + "Value"); // outdated
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
